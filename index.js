55de15c09572b9e5e0e31680af9779ace3497ddf
Cameron Bytheway <cameron@nujii.com>
2013-02-06 19:12:04 -0700

    Cache the clients

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var riemann = require("riemann")
   , domain = require("domain").create();
 
 /**
+ * Cache the clients
+ * @api private
+ */
+var CACHE = {};
+
+/**
  * Simple Riemann Client
  *
  * @param {Object|Function} options
@@ -18,8 +24,10 @@ module.exports = function(options, cb) {
   };
 
   // Defaults
-  options.host = options.host || process.env.RIEMANN_HOST;
-  options.port = options.port || process.env.RIEMANN_PORT;
+  options.host = options.host || process.env.RIEMANN_HOST || "127.0.0.1";
+  options.port = options.port || process.env.RIEMANN_PORT || 5555;
+
+  if (CACHE[options.host+options.port]) return CACHE[options.host+options.port];
 
   domain.on("error", cb);
 
@@ -40,6 +48,8 @@ module.exports = function(options, cb) {
       domain.dispose();
     };
   });
+
+  if(client) CACHE[options.host+options.port] = client;
 
   return client;
 };
